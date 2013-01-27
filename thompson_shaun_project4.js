ce878db8e0ccc9a50dcf61e3db61b9182697707f
Shaun Thompson <shaunthompson@fullsail.edu>
2013-01-27 12:49:33 -0500

    Finished URL boolean.

--- thompson_shaun_project4.js
+++ thompson_shaun_project4.js
@@ -23,14 +23,14 @@ var myLibrary = function() {
 	};
 	
 	// Does a string follow an aaa@bbb.ccc pattern like an email address?
-	var checkEmail = function(url){
-		var at = url.indexOf("@"),
-			com = url.lastIndexOf(".com"),
-			username = url.substring(0, at),
-			server = url.substring(at+1, com),
+	var checkEmail = function(address){
+		var at = address.indexOf("@"),
+			com = address.lastIndexOf(".com"),
+			username = address.substring(0, at),
+			server = address.substring(at+1, com),
 			email = username + "@" + server + ".com";
 		
-		if (url === email){
+		if (address === email){
 			return true;
 		} else {
 			return false;
@@ -40,6 +40,24 @@ var myLibrary = function() {
 	};
 
 	// Is the string a URL? (Does it start with http: or https:?)
+	var checkURL = function(url){
+		var hypertext = url.lastIndexOf("/"),
+			protocol = url.substring(0, hypertext+1)
+			page = url.substring(hypertext+1, url.length);
+				
+		if(protocol === "https://" || protocol === "http://"){
+			return true;
+		} else {
+			return false;
+		
+		};
+
+	};
+		
+
+
+
+
 /*	var checkURL = function(url) {
 		var http = 	(number.substring(0,8)),
 			https = (number.substring(0,9));
@@ -114,7 +132,7 @@ var myLibrary = function() {
 	return {
 		"checkPhone": checkPhone,
 		"checkEmail": checkEmail,
-/*		"checkURL": checkURL, */
+		"checkURL": checkURL, 
 		"checkNumeric": checkNumeric,
 		"areYouLee": 	areYouLee,
 		"checkString":	checkString
@@ -128,7 +146,7 @@ var newLib = new myLibrary();
 
 console.log("Is this a phone number?: " + newLib.checkPhone("111-111-1111"));
 console.log("Is this an email address?: " + newLib.checkEmail("ShaunThompson@live.com"));
-/* console.log("Is this a URL address?: " + newLib.checkURL("http://iam.com")); */
+console.log("Is this a URL address?: " + newLib.checkURL("https://success.com")); 
 console.log("Is this a number?: " + newLib.checkNumeric(156));
 console.log("Is this Lee?: " + newLib.areYouLee("Lee"));
 console.log("The dash is in the position: " + newLib.checkString("123-456"));
